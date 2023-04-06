c5741def1eb75da9e73943ad6d2817ec375de5bc
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-05 22:42:35 -0700

    added current date and fixed table

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+
+// function for timer 
+$(document).ready(function (){
+    setInterval(function(){
+    $("#currentDay").text(dayjs().format("dddd, MMMM D YYYY, h:mm:ss a"));
+    }, 1000);
+    });
