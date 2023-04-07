34cebd5015aa81f74d966d0edb0ac8b7fb3d04d1
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-06 17:16:55 -0700

    merging

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,10 @@ $(document).ready(function (){
     $("#currentDay").text(dayjs().format("dddd, MMMM D YYYY, h:mm:ss a"));
     }, 1000);
     });
+
+    $(function () {
+        $('#datepicker').datepicker({
+          changeMonth: true,
+          changeYear: true,
+        });
+      });
